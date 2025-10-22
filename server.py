e2c7f915d07553d258bf6cfda604cee120956836
abocardoq <43285890+abocardoq@users.noreply.github.com>
2025-10-22 14:53:14 -0600

    Add files via upload

---
+++ server.py
@@ -0,0 +1,17 @@
+# server.py
+from fastmcp import FastMCP
+
+mcp = FastMCP("Demo 🚀")
+
+@mcp.tool
+def suma(a: int, b: int) -> int:
+    """Suma dos numeros"""
+    return a + b
+
+@mcp.tool
+def multiplica(a: float, b: float) -> float:
+    """Multiplica dos numeros"""
+    return a * b
+
+if __name__ == "__main__":
+    mcp.run()
