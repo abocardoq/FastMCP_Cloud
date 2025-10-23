32d1044a3aa8cb5e73b887c02a9547ad7f15cdd9
abocardoq <43285890+abocardoq@users.noreply.github.com>
2025-10-22 18:20:20 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -13,5 +13,10 @@ def multiplica(a: float, b: float) -> float:
     """Multiplica dos numeros"""
     return a * b
 
+@mcp.tool
+def divide(a: float, b: float) -> float:
+    """Divide dos numeros"""
+    return a / b
+
 if __name__ == "__main__":
     mcp.run()
